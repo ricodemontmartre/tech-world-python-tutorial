cc3a57dcc059094de630bcc2db209aa0cf94f0be
Federico Desmoulin <ricodemontmartre@Federicos-MacBook-Pro.local>
2025-06-12 09:27:24 +0200

    Variables in Python

---
+++ main.py
@@ -0,0 +1,5 @@
+calcution_to_units: int = 24 * 60
+
+print("20 days is " + str(20 * calcution_to_units) + " minutes")
+print(f"20 days is {20 * calcution_to_units} minutes") # f = format
+print(f"30 days is {30 * calcution_to_units} minutes")
