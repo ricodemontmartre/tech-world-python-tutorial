c5fdfc9e7bf1be72b2eb2ef0129f39d97a02ca11
Federico Desmoulin <ricodemontmartre@Federicos-MacBook-Pro.local>
2025-06-18 18:48:57 +0200

    Object Oriented Programming: Classes and Objects

--- main.py
+++ main.py
@@ -1,48 +1,17 @@
-import openpyxl
+from post import Post
+from user import User
 
-inv_file = openpyxl.load_workbook("inventory.xlsx")
-print(inv_file["Sheet1"])
-product_list = inv_file["Sheet1"]
+user_one = User("email@mail.com", "Rico", 'pwd', "Dev")
+user_one.get_user_info()
 
-products_by_supplier = {}
-total_value_per_supplier = {}
-products_under_10_inv = {}
+user_one.change_job_title("test")
+user_one.get_user_info()
 
-print(product_list.max_row)
+user_two = User("email2@mail.com", "Math", 'mdp', "Runner")
+user_two.get_user_info()
 
-for product_row in range(2, product_list.max_row + 1):
-    supplier_name = product_list.cell(product_row, 4).value
-    inventory = product_list.cell(product_row, 2).value
-    price = product_list.cell(product_row, 3).value
-    product_number = product_list.cell(product_row, 1).value
-    inventory_price = product_list.cell(product_row, 5)
-    print(supplier_name)
+post_one = Post("post one", "me")
+post_one.get_post_info()
 
-    # Number of products by supplier
-    if supplier_name in products_by_supplier:
-        current_num_product = products_by_supplier.get(supplier_name)
-        products_by_supplier[supplier_name] = current_num_product + 1
-    else:
-        print("new supplier added")
-        products_by_supplier[supplier_name] = 1
-
-    # Total value of products by suppliers
-    if supplier_name in total_value_per_supplier:
-        current_total_value = total_value_per_supplier.get(supplier_name)
-        total_value_per_supplier[supplier_name] = total_value = current_total_value + price * inventory
-    else:
-        total_value_per_supplier[supplier_name] = price * inventory
-
-
-    # Calculation inventory less 10
-    if inventory < 10:
-        products_under_10_inv[int(product_number)] = int(inventory)
-
-    # Value for total inventory price
-    inventory_price.value = inventory * price
-
-print(products_by_supplier)
-print(total_value_per_supplier)
-print(products_under_10_inv)
-
-inv_file.save("inventory_with_total_product_price.xlsx")
+post_two = Post("post one", user_two.name)
+post_two.get_post_info()
---
+++ user.py
@@ -0,0 +1,16 @@
+class User:
+    def __init__(self, email, name, password, current_job_title):
+        self.email = email
+        self.name = name
+        self.password = password
+        self.current_job_title = current_job_title
+
+    def check_password(self, new_password):
+        self.password = new_password
+
+    def change_job_title(self, new_job_title):
+        self.current_job_title = new_job_title
+
+    def get_user_info(self):
+        print(
+            f"Email: {self.email}, Name: {self.name}, Password: {self.password}, Current Job Title: {self.current_job_title}")
