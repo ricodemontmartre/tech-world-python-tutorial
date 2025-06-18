b1b55d799b905d34a53fa8f502c71f4b04213996
Federico Desmoulin <ricodemontmartre@Federicos-MacBook-Pro.local>
2025-06-18 17:26:45 +0200

    Project: Automation with Python (Working with Spreadsheets) - Excercise 2

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ print(inv_file["Sheet1"])
 product_list = inv_file["Sheet1"]
 
 products_by_supplier = {}
+total_value_per_supplier = {}
 
 print(product_list.max_row)
 
 for product_row in range(2, product_list.max_row + 1):
     supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
     print(supplier_name)
 
+    # Number of products by supplier
     if supplier_name in products_by_supplier:
         current_num_product = products_by_supplier.get(supplier_name)
         products_by_supplier[supplier_name] = current_num_product + 1
@@ -19,4 +23,14 @@ for product_row in range(2, product_list.max_row + 1):
         print("new supplier added")
         products_by_supplier[supplier_name] = 1
 
+    # Total value of products by suppliers
+    if supplier_name in total_value_per_supplier:
+        current_total_value = total_value_per_supplier.get(supplier_name)
+        total_value_per_supplier[supplier_name] = total_value = current_total_value + price * inventory
+    else:
+
+        total_value_per_supplier[supplier_name] = price * inventory
+
+
 print(products_by_supplier)
+print(total_value_per_supplier)
